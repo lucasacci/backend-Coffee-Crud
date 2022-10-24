2309fe337179234c9473023083c6437e0795b8f3
Luciano <lucasacci@hotmail.com>
2022-10-24 11:04:09 -0300

    agregamos el modelo de producto para mandarlo a base de datos

---
+++ src/models/producto.js
@@ -0,0 +1,30 @@
+
+
+import mongoose,{mongo, Schema} from "mongoose";
+
+const productoSchema = new Schema({
+    nombreProducto: {
+        type: String,
+        required: true,
+        unique: true,
+        maxLength:50,
+        minLength: 2,
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    imagen:{
+        type: String,
+        required: true
+    },
+    categoria:{
+        type: String,
+        required: true
+    }
+
+})
+
+const Producto = mongoose.model('producto')
+
+export default Producto;
