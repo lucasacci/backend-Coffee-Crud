add79f8d3e97e8c40c1cb0304c782721307a129c
Luciano <lucasacci@hotmail.com>
2022-10-31 11:30:30 -0300

    agregamos el archivo de configuracion de vercel

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose, { mongo, Mongoose } from "mongoose";
 
-const url = `mongodb://localhost:27017/coffee-crud`;
+// const url = `mongodb://localhost:27017/coffee-crud`;
+const url = `mongodb+srv://Lucasacci:qliadademierda@cluster0.l5htrji.mongodb.net/cafe-benito-santos`;
 
 
 mongoose.connect(url);
