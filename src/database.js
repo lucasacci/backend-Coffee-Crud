3062c9de038350a0f9493e10048d553c706c9142
Luciano <lucasacci@hotmail.com>
2022-12-26 16:42:29 -0300

    cambios en los endpoints de base de daot

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo, Mongoose } from "mongoose";
 
 // const url = `mongodb://localhost:27017/coffee-crud`;
-const url = `mongodb+srv://lucasacci:RMfXXIF1ka3l487h@clustercafecrud.1ovnlam.mongodb.net/test`;
+const url = `mongodb+srv://lucasacci:RMfXXIF1ka3l487h@clustercafecrud.1ovnlam.mongodb.net/cafeDBcrud`;
 
 
 mongoose.connect(url);
