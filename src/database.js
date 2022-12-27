0ac99dbf3d0de497a47e175f73090a0ec45d4d18
Luciano <lucasacci@hotmail.com>
2022-12-27 14:14:31 -0300

    cambio en base de datos

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo, Mongoose } from "mongoose";
 
 // const url = `mongodb://localhost:27017/coffee-crud`;
-const url = `mongodb+srv://jony:nDXq7Hu1kLQLLA4d@clustercomision7i.3qcgclh.mongodb.net/cafe-benitos-santos`;
+const url = `mongodb+srv://usuario_cafe:eNuzb1qZ6ueV59Vd@clustercafecrud.b4skld8.mongodb.net/cafe-crud`;
 
 
 mongoose.connect(url);
